2ecb53cbf50793a71eb06bd1ed991eeffacd5e67
Michael Venetz <105941997+Michtz@users.noreply.github.com>
2026-02-12 18:18:36 +0100

    added insta icon

--- src/app/components/section/header/Header.tsx
+++ src/app/components/section/header/Header.tsx
@@ -7,6 +7,7 @@ import LoadingSpinner from "@/app/components/system/loadingSpinner/LoadingSpinne
 import SideNav from "@/app/components/system/sideNav/SideNav";
 import HamburgerIcon from "@/app/components/icons/HamburgerIcon";
 import LogoIcon from "@/app/components/icons/Logo";
+import InstagramIcon from "@/app/components/icons/InstagramIcon";
 
 const ResponsiveAppBar = () => {
   const [isSideNavOpen, setIsSideNavOpen] = useState(false);
@@ -37,6 +38,7 @@ const ResponsiveAppBar = () => {
             <li className={style.navItem}>
               <Link href={"/about"}>Über uns</Link>
             </li>
+            <li className={style.navItem}></li>
           </ul>
         </div>
 
@@ -44,9 +46,11 @@ const ResponsiveAppBar = () => {
           <LogoIcon height={60} />
         </a>
 
-        <span
-          className={`${style.rightNavContainer} ${!isLoading ? style.fadeIn : style.fadeOut}`}
-        ></span>
+        <span className={`${style.rightNavContainer} ${!isLoading ? style.fadeIn : style.fadeOut}`}>
+          <Link href={"https://www.instagram.com/meier_technik_gmbh?igsh=MWhuam0waXQzYnpndA=="}>
+            <InstagramIcon />
+          </Link>
+        </span>
       </header>
 
       <SideNav isOpen={isSideNavOpen} onClose={closeSideNav} />
